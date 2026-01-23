dea7adb455ec704cb38dc48327bbd78b31cff883
Letícia <leeharaujo@icloud.com>
2026-01-22 21:25:05 -0300

    created sign up

---
+++ frontend/frontend/src/components/SignIn.jsx
@@ -0,0 +1,31 @@
+const SignIn = () => {
+    return(
+        <div className="login-content">     
+            <div className="login-title">
+                <h1>Bem-vindo de volta!</h1>
+                <p>Entre para acessar suas viagens</p>
+            </div>
+            <div className="login-form">
+                <form className="form">
+                    <label htmlFor="email">Email</label>
+                    <div className='input-wrapper'>
+                        <i class="fa-regular fa-envelope icon-input"></i>
+                        <input type="email" name="email" id="email" placeholder="Digite seu email"/>
+                    </div>
+                    <label htmlFor="senha">Senha</label>
+                    <div className="input-wrapper">
+                        <i class="fa-solid fa-lock icon-input"></i>
+                        <input type="password" name="senha" id="senha" placeholder="Digite sua senha"/>
+                    </div>
+                    <button type="submit" className="btn">Entrar</button>
+                </form>
+            </div>
+            <div className="login-text">
+                <p>Não tem uma conta?<a href="#"> Cadastre-se</a></p>
+                <a href="#" className="password-text">Esqueceu sua senha?</a>
+            </div>
+        </div>
+    )
+}
+
+export default SignIn;
---
+++ frontend/frontend/src/components/SignUp.jsx
@@ -0,0 +1,35 @@
+const SignUp = () => {
+    return(
+        <div className="login-content">     
+            <div className="login-title">
+                <h1>Criar conta</h1>
+                <p>Comece a dividir despesas agora</p>
+            </div>
+            <div className="login-form">
+                <form className="form">
+                    <label htmlFor="name">Nome completo</label>
+                    <div className="input-wrapper">
+                        <i class="fa-regular fa-user icon-input"></i>
+                        <input type="text" name="name" id="name" placeholder="Digite seu nome completo"/>
+                    </div>
+                    <label htmlFor="email">Email</label>
+                    <div className='input-wrapper'>
+                        <i class="fa-regular fa-envelope icon-input"></i>
+                        <input type="email" name="email" id="email" placeholder="Digite seu email"/>
+                    </div>
+                    <label htmlFor="senha">Senha</label>
+                    <div className="input-wrapper">
+                        <i class="fa-solid fa-lock icon-input"></i>
+                        <input type="password" name="senha" id="senha" placeholder="Digite sua senha"/>
+                    </div>
+                    <button type="submit" className="btn">Criar conta</button>
+                </form>
+            </div>
+            <div className="login-text">
+                <p>Já tem uma conta?<a href=""> Faça login</a></p>
+            </div>
+        </div>
+    )
+}
+
+export default SignUp;
