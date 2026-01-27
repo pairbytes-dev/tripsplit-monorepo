b93895483ead1eb41a8ebd82e6a288606da949f6
Letícia <leeharaujo@icloud.com>
2026-01-26 22:20:10 -0300

    homepage

--- frontend/frontend/src/components/home/HomeStart.jsx
+++ frontend/frontend/src/components/home/HomeStart.jsx
@@ -2,12 +2,12 @@ const HomeStart = () => {
     return (
         <div className="div-home-start">
             <section className="section-start">
-                <h1>Comece a dividir suas despesas. Viaje sem dor de cabeça na hora de dividir.</h1>
-                <p>Divida despesas de viagens de forma justa, automática e transparente. Registre gastos, veja relatórios em tempo real e acerte as contas em segundos.</p>
-                <button className="btn-home">Começar agora</button>
+                <p className="first-txt-home">Comece a dividir suas despesas. Viaje sem dor de cabeça na hora de dividir.</p>
+                <p className="second-txt-home">Divida despesas de viagens de forma justa, automática e transparente. Registre gastos, veja relatórios em tempo real e acerte as contas em segundos.</p>
+                <button className="btn-home" id="start-now">Começar agora</button>
             </section>
             <section className="section-start">
-                <p>Olá</p>
+                <div></div>
             </section>
         </div>
     )
