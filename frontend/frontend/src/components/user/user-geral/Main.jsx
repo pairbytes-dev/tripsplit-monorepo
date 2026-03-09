14e1697700183283e08577a2f483525762d64b3a
Letícia <leeharaujo@icloud.com>
2026-03-09 09:44:35 -0300

    style - modal

--- frontend/frontend/src/components/user/user-geral/Main.jsx
+++ frontend/frontend/src/components/user/user-geral/Main.jsx
@@ -1,6 +1,11 @@
 import mais from '../../../assets/img/mais.png'
+import { useState } from 'react';
+import Modal from './Modal.jsx';
 
 const Main = () => {
+
+    const [openModal, setOpenModal] = useState(false);
+
     return (
         <div className="user-main"> 
             <div className='user-main-div-title'>
@@ -8,7 +13,7 @@ const Main = () => {
                     <h1 className="title-main-user">Meus Grupos</h1>
                     <p>Gerencie suas despesas compartilhadas</p>
                 </div>
-                <button className="btn-home btn-header" id='btn-green'>
+                <button className="btn-home btn-header" id='btn-green' onClick={() => setOpenModal(true)}>
                     <img src={mais} alt="icone de mais" className='icon-header'/>
                     Novo grupo
                 </button>
@@ -16,6 +21,8 @@ const Main = () => {
             <div>
                 
             </div>
+
+            <Modal isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)} />
         </div>
     )
 }
--- frontend/frontend/src/components/user/user-geral/Modal.jsx
+++ frontend/frontend/src/components/user/user-geral/Modal.jsx
@@ -4,27 +4,39 @@ export default function Modal({ isOpen, setModalOpen}){
     return (
         <div className="background-modal">
             <div className="main-div-modal">
-                <section>
+                <section className="section-modal">
                     <div>
-                        <h1>Criar Novo Grupo</h1>
+                        <h1 className="title-modal">Criar Novo Grupo</h1>
                         <p>Informações do grupo</p>
                     </div>
-                    <button onClick={setModalOpen}>Fechar</button>
+                    <button onClick={setModalOpen} className="btn-modal-close"><i class="fa-solid fa-x"></i></button>
                 </section>
-                <section>
-                    <form action="get">
+                <section className="section-modal-2">
+                    <form action="get" className="form-modal">
                         <label htmlFor="nome">Nome da despesa</label>
-                        <input type="text" name="nome" id="nome"/>
+                        <div className="div-modal-form"><input type="text" name="nome" id="nome" placeholder="Ex: Viagem Ubatuba 2026"/></div>
 
                         <label htmlFor="categoria">Categoria</label>
-                        <input type="range" name="categoria" id="categoria" />
+                        <div className="div-modal-form">
+                            <select name="categoria" id="categoria">
+                                <option value="">Selecione uma categoria</option>
+                                <option value="alimentacao">Alimentação</option>
+                                <option value="transporte">Transporte</option>
+                                <option value="viagem">Viagem</option>
+                                <option value="lazer">Lazer</option>
+                                <option value="saude">Saúde</option>
+                                <option value="outros">Outros</option>
+                            </select>
+                        </div>
 
                         <label htmlFor="descricao">Descrição</label>
-                        <input type="text" name="descricao" id="descricao" />
+                        <div className="div-modal-form"><textarea name="descricao" id="descricao"></textarea></div>
                     </form>
 
-                    <button onClick={setModalOpen}>Fechar</button>
-                    <button onClick={setModalOpen}>Criar Grupo</button>
+                    <div className="div-btn-modal">
+                        <button onClick={setModalOpen} className="btn-home btn-close btn-size">Fechar</button>
+                        <button onClick={setModalOpen} className="btn-home btn-size">Criar Grupo</button>
+                    </div>
                 </section>
             </div>
                 
